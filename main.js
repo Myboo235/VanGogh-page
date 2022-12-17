f5ea7604959fe36833996efac741b46422d5804a
Nhut <vivuvivunice@gmail.com>
2022-12-17 08:41:19 +0700

    Add-legacy,fix-form

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-let user = {
-    name : "Nhut",
-    age : 20
+let star = document.querySelectorAll('input');
+let showValue = document.querySelector('#rating-value');
+
+for (let i = 0; i < star.length; i++) {
+	star[i].addEventListener('click', function() {
+		i = this.value;
+
+		showValue.innerHTML = i + " out of 5";
+	});
 }
-console.log(user)
